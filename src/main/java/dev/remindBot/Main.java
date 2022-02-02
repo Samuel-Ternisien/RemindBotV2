ed0aa07a2e0e780ad530b2e9cc2a89596769bb50
samuel Ternisien <samuel_ternisien@ens-univ.artois.fr>
2022-02-02 17:59:04 +0100

    Comments

--- src/main/java/dev/remindBot/Main.java
+++ src/main/java/dev/remindBot/Main.java
@@ -7,24 +7,33 @@ import org.javacord.api.DiscordApiBuilder;
 
 import java.io.File;
 
+/**
+ * Main classes to run the bot with a listener
+ * Running this main method connect the bot and give it ability to execute command registered in CommandRegistry static method
+ */
 public class Main {
 
+    //attrs
     private static DiscordApi api;
     private static configManager configManager;
 
 
     public static void main(String[] args) {
-
+        // Init the configManager which allows use to get the bot properties through all our classes
         configManager = new configManager(new File(System.getProperty("user.dir"),"config.toml"));
 
+        // Init out bot and get the bot token through the configManager
         api= new DiscordApiBuilder()
                 .setToken(configManager.getToml().getString("bot.token"))
                 .login().join();
 
+        // Add a listener for the bot { read the comments on MessageManager classes for more information }
         api.addMessageCreateListener(MessageManager::create);
 
     }
 
+
+    //Getter
     public static dev.remindBot.Utils.configManager getConfigManager() {
         return configManager;
     }
